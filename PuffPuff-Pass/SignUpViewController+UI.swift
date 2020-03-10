4937f5a7ef20811e5f8b49535df9fd8109715c03
Adi Arquero <adrian@arquerostudios.com>
2020-03-10 09:17:48 -0700

    CocoaPods completely removed and reinstalled to project due to runTime Error // Firebase reinstall // PhotoPicker wired to database

--- PuffPuff-Pass/SignUpViewController+UI.swift
+++ PuffPuff-Pass/SignUpViewController+UI.swift
@@ -12,18 +12,30 @@ extension SignUpViewController {
     
     func setupTitlelabel(){
         let title = "Sign Up"
-          
-           
-           let attributedText = NSMutableAttributedString(string: title, attributes: [NSAttributedString.Key.font: UIFont.init(name: "Didot", size: 28)!, NSAttributedString.Key.foregroundColor : UIColor.black
-           ])
-          
-           titleTextLabel.attributedText = attributedText
+        
+        
+        let attributedText = NSMutableAttributedString(string: title, attributes: [NSAttributedString.Key.font: UIFont.init(name: "Didot", size: 28)!, NSAttributedString.Key.foregroundColor : UIColor.black
+        ])
+        
+        titleTextLabel.attributedText = attributedText
         
     }
     
     func setupAvatar(){
         avatar.layer.cornerRadius = 40
         avatar.clipsToBounds = true
+        
+        avatar.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(presentPicker))
+        avatar.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func presentPicker() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
     }
     
     func setupFullNameTextField() {
@@ -44,52 +56,67 @@ extension SignUpViewController {
     func setupEmailTextField() {
         emailContainerView.layer.borderWidth = 1
         emailContainerView.layer.borderColor = UIColor(red: 210/255, green: 210/255, blue: 210/255, alpha: 1).cgColor
-               emailContainerView.layer.cornerRadius = 3
-               emailContainerView.clipsToBounds = true
-               
-               emailTextField.borderStyle = .none
-               
-               
-               let placeholderAttr = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor : UIColor(red: 170/255, green: 170/255, blue: 170/255, alpha: 1)])
-               emailTextField.attributedPlaceholder = placeholderAttr
+        emailContainerView.layer.cornerRadius = 3
+        emailContainerView.clipsToBounds = true
+        
+        emailTextField.borderStyle = .none
+        
+        
+        let placeholderAttr = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor : UIColor(red: 170/255, green: 170/255, blue: 170/255, alpha: 1)])
+        emailTextField.attributedPlaceholder = placeholderAttr
         
     }
     
     func setupPasswrdTextField() {
         passwordContainerView.layer.borderWidth = 1
         passwordContainerView.layer.borderColor = UIColor(red: 210/255, green: 210/255, blue: 210/255, alpha: 1).cgColor
-               passwordContainerView.layer.cornerRadius = 3
-               passwordContainerView.clipsToBounds = true
-               
-               passwordTextField.borderStyle = .none
-               
-               
-               let placeholderAttr = NSAttributedString(string: "Password (6+ Characters)", attributes: [NSAttributedString.Key.foregroundColor : UIColor(red: 170/255, green: 170/255, blue: 170/255, alpha: 1)])
-               passwordTextField.attributedPlaceholder = placeholderAttr
+        passwordContainerView.layer.cornerRadius = 3
+        passwordContainerView.clipsToBounds = true
+        
+        passwordTextField.borderStyle = .none
+        
+        
+        let placeholderAttr = NSAttributedString(string: "Password (6+ Characters)", attributes: [NSAttributedString.Key.foregroundColor : UIColor(red: 170/255, green: 170/255, blue: 170/255, alpha: 1)])
+        passwordTextField.attributedPlaceholder = placeholderAttr
         
     }
     
     func setupSignUpButton(){
         signUpButton.setTitle("Sign Up", for: UIControl.State.normal)
-                      signUpButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
-                      signUpButton.backgroundColor = UIColor.black
-                      signUpButton.layer.cornerRadius = 5
-                      signUpButton.clipsToBounds = true
+        signUpButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        signUpButton.backgroundColor = UIColor.black
+        signUpButton.layer.cornerRadius = 5
+        signUpButton.clipsToBounds = true
         signUpButton.setTitleColor(.white, for: UIControl.State.normal)
         
     }
     
     func setupSignInButton(){
         let attributedText = NSMutableAttributedString(string: "Already have an Account? ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 0.65)
-                           ])
-                      
+        ])
+        
         let attributedSubText = NSMutableAttributedString(string: "Sign In", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18), NSAttributedString.Key.foregroundColor : UIColor.black
-                      ])
-                      attributedText.append(attributedSubText)
+        ])
+        attributedText.append(attributedSubText)
         signInButton.setAttributedTitle(attributedText, for: UIControl.State.normal)
-                      
         
-                      
+    }
+}
+
+
+extension SignUpViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imageSelected = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            image = imageSelected
+            
+            avatar.image = imageSelected
+        }
         
+        if let imageOriginal = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            image = imageOriginal
+            avatar.image = imageOriginal
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
     }
 }
--- PuffPuff-Pass/SignUpViewController.swift
+++ PuffPuff-Pass/SignUpViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 import FirebaseAuth
 import FirebaseDatabase
+import FirebaseStorage
 
 class SignUpViewController: UIViewController {
-
-  
+    
+    
     
     @IBOutlet weak var titleTextLabel: UILabel!
     @IBOutlet weak var avatar: UIImageView!
@@ -25,6 +26,7 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var signInButton: UIButton!
     
+    var image: UIImage? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,13 +43,24 @@ class SignUpViewController: UIViewController {
         setupSignUpButton()
         setupSignInButton()
     }
-
+    
     @IBAction func dismissAction(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func signUpButtonDidTapped(_ sender: Any) {
-        Auth.auth().createUser(withEmail: "test003@gmail.com", password: "123456") { (authDataResult, error) in
+        
+        guard let imageSelected = self.image else {
+            print("Avatar is nil")
+            return
+        }
+        guard let imageData = imageSelected.jpegData(compressionQuality: 0.4) else {
+            return
+        }
+        
+        
+        
+        Auth.auth().createUser(withEmail: "test004@gmail.com", password: "123456") { (authDataResult, error) in
             if error != nil {
                 print(error!.localizedDescription)
                 return
@@ -58,10 +71,36 @@ class SignUpViewController: UIViewController {
                     "uid": authData.user.uid,
                     "email": authData.user.email,
                     "profileImageUrl": "",
-                    "status": ""
+                    "status": "Welcome to PuffPuff-Pass"
                 ]
                 
-                Database.database().reference().child("users").child(authData.user.uid).updateChildValues(dict, withCompletionBlock: { (error, ref) in
+                let storageRef = Storage.storage().reference(forURL: "gs://puffpuff-pass-341b3.appspot.com")
+                let storageProfileRef = storageRef.child("profile").child(authData.user.uid)
+                
+                let metadata = StorageMetadata()
+                metadata.contentType = "image/jpg"
+                // might need a completion here
+                storageProfileRef.putData(imageData, metadata: metadata, completion: { (storageMetaData, error) in
+                    if error != nil {
+                        print(error?.localizedDescription)
+                        return
+                    }
+                })
+                
+                //                let storageRef = Storage.storage().reference(forURL:"https://puffpuff-pass-341b3.firebaseio.com/")
+                //                let storageProfileRef = storageRef.child("profile").child(authData.user.uid)
+                //
+                //                let metadata = StorageMetadata()
+                //                metadata.contentType = "image/jpg"
+                //                storageProfileRef.putData(imageData, metadata: metadata, completion: { (storageMetaData, error) in
+                //                    if error != nil {
+                //                        print(error?.localizedDescription)
+                //                        return
+                //                }
+                //
+                //
+                //                })
+                Database.database().reference().child("users").child(authData.user.uid).updateChildValues(dict,withCompletionBlock: { (error, ref) in
                     if error == nil {
                         print("Done")
                     }
