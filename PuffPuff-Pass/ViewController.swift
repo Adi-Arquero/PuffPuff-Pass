a1d917e060da6931e881695245317443f33d373c
Adi Arquero <adrian@arquerostudios.com>
2020-03-10 03:07:32 -0700

    removed main.storyboard >> added Welcome.storyboard  >>  Added initial button layout for Welcome Screen

--- PuffPuff-Pass/ViewController.swift
+++ PuffPuff-Pass/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var signInFacebookButton: UIButton!
+    @IBOutlet weak var signInGoogleButton: UIButton!
+    @IBOutlet weak var createAccountButton: UIButton!
+    @IBOutlet weak var termsOfServiceLabel: UILabel!
+    @IBOutlet weak var orLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
-
+    
+    func setupUI() {
+        let title = "Create a new account"
+        let subTitle = "\n Yo Mama is so fat..."
+        
+        let attributedText = NSMutableAttributedString(string: title, attributes: [NSAttributedString.Key.font: UIFont.init(name: "Didot", size: 28)!, NSAttributedString.Key.foregroundColor : UIColor.black
+        ])
+        let attributedSubTitle = NSMutableAttributedString(string: subTitle, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 0.45)
+        ])
+        
+        attributedText.append(attributedSubTitle)
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 5
+        
+        attributedText.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedText.length))
+        titleLabel.numberOfLines = 0
+        titleLabel.attributedText = attributedText
+    }
+    
+    
 }
 
